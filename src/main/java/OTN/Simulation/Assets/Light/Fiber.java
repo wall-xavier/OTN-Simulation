ed8e641ecc0e20f2f03e654d7ed452031530a416
wall-xavier <xvwall@gmail.com>
2025-11-09 15:28:54 -0700

    Added Fiber A and Z side transponders:

--- src/main/java/OTN/Simulation/Assets/Light/Fiber.java
+++ src/main/java/OTN/Simulation/Assets/Light/Fiber.java
@@ -1,15 +1,19 @@
 package OTN.Simulation.Assets.Light;
-
+import OTN.Simulation.Assets.Cards.Transponder.Assets.Transponder;
 public class Fiber {
     
     double length;
     final double ATTENUATION = .2;
     String label;
+    Transponder ASide;
+    Transponder ZSide;
 
-    public Fiber(double len, String lb){
+    public Fiber(double len, String lb, Transponder A, Transponder Z){
 
         length = len;
         label = lb;
+        ASide = A;
+        ZSide = Z;
     }
 
     public String getLabel(){
