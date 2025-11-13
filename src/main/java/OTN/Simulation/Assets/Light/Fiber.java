dd9ba23b14eb75370f3470f803a472f9f7cc22a9
wall-xavier <xvwall@gmail.com>
2025-11-13 10:56:28 -0700

    Added licensing information.

--- src/main/java/OTN/Simulation/Assets/Light/Fiber.java
+++ src/main/java/OTN/Simulation/Assets/Light/Fiber.java
@@ -1,3 +1,15 @@
+/*
+
+This file is part of OTN-Simulation.
+
+OTN-Simulation is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+OTN-Simulation is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with OTN-Simulation. If not, see <https://www.gnu.org/licenses/>. 
+
+*/
+
 package OTN.Simulation.Assets.Light;
 import OTN.Simulation.Assets.Cards.Transponder.Assets.Transponder;
 public class Fiber {
